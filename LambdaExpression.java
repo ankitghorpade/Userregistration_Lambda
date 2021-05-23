d508fd7e2f53167ef823bd8878dcac2360e59b93
unknown <ankitghorpade59@gmail.com>
2021-05-23 03:43:19 -0800

    mobile validation uc4

--- LambdaExpression.java
+++ LambdaExpression.java
@@ -12,22 +12,27 @@ public class LambdaExpression {
 
 	@Test
 	public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter First Name: ");
-		String firstName = s.next();
+		String firstName = sc.next();
         Lamda Name=(String value) -> Pattern.matches("^[A-Z a-z]{3,25}$",firstName);
 		System.out.println(Name.eval("value"));
 		
 		System.out.println("Enter Last Name: ");
-		String LastName = s.next();
+		String LastName = sc.next();
         Lamda Lastname=(String value) -> Pattern.matches("^[A-Z a-z]{3,25}$",LastName);
 		System.out.println(Lastname.eval("value"));
 		
-		  System.out.println("Enter Email : ");
-	      String Email1 = s.next();
-	      Lamda Email=(String value) -> Pattern.matches("^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,6}(\\.[A-Za-z]{2,6})?$",Email1);
-			System.out.println(Email.eval("value"));
+    	 System.out.println("Enter Email : ");
+	     String Email1 = sc.next();
+	     Lamda Email=(String value) -> Pattern.matches("^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,6}(\\.[A-Za-z]{2,6})?$",Email1);
+	   	System.out.println(Email.eval("value"));
+			
+			System.out.println("Enter mobile number : ");
+	        String Mobile1 = sc.next();
+	        Lamda Mobile=(String value) -> Pattern.matches("^[A-Z a-z]{3,25}$",Mobile1);
+			System.out.println(Mobile.eval("value"));
 	}
 
 }
